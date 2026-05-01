ab8ac633660f879bde6cad29e1d2effe496519a7
MasterDevopper <thedevops112th@gmail.com>
2026-04-30 22:49:28 -0400

    Sprint 15B Pass 10: add T2V history browser foundation

---
+++ qt_ui/T2VHistoryPage.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <QList>
+#include <QString>
+#include <QWidget>
+
+class QLabel;
+class QPushButton;
+class QTableWidget;
+class QTableWidgetItem;
+
+class T2VHistoryPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit T2VHistoryPage(QWidget *parent = nullptr);
+
+    void setProjectRoot(const QString &projectRoot);
+    void refreshHistory();
+
+protected:
+    void showEvent(QShowEvent *event) override;
+
+private slots:
+    void handleSelectionChanged();
+    void openSelectedVideo();
+    void revealSelectedVideo();
+
+private:
+    struct VideoHistoryItem
+    {
+        QString historyId;
+        QString jobId;
+        QString command;
+        QString promptPreview;
+        QString outputPath;
+        QString metadataPath;
+        QString finishedAt;
+        QString durationLabel;
+        QString resolution;
+        QString stackSummary;
+        QString lowModelName;
+        QString highModelName;
+        QString runtimeSummary;
+        QString metadataStatus;
+        QString outputContractStatus;
+        qint64 outputFileSizeBytes = 0;
+        bool outputExists = false;
+        bool metadataExists = false;
+        bool outputContractOk = false;
+    };
+
+    QString historyIndexPath() const;
+    QList<VideoHistoryItem> loadHistoryItems() const;
+    void populateTable();
+    void updateDetailsForItem(const VideoHistoryItem &item);
+    void updateEmptyDetails();
+    int selectedRow() const;
+    const VideoHistoryItem *selectedItem() const;
+    QString formatFileSize(qint64 bytes) const;
+    QString formatFinishedAt(const QString &isoText) const;
+    QString compactText(const QString &text, int maxChars) const;
+    void applyTheme();
+
+    QString projectRoot_;
+    QList<VideoHistoryItem> items_;
+
+    QLabel *summaryLabel_ = nullptr;
+    QLabel *detailsTitleLabel_ = nullptr;
+    QLabel *detailsBodyLabel_ = nullptr;
+    QLabel *detailsStatusLabel_ = nullptr;
+    QTableWidget *table_ = nullptr;
+    QPushButton *refreshButton_ = nullptr;
+    QPushButton *openVideoButton_ = nullptr;
+    QPushButton *revealFolderButton_ = nullptr;
+};
